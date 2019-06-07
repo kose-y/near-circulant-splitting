c545b36119b966e7a3061904c927d31c03871f53
Ernest Ryu <eryu@math.ucla.edu>
2019-06-07 10:23:57 -0700

    updated cone beam experiments

--- cone_beam_temp.m
+++ cone_beam_temp.m
@@ -1,6 +1,6 @@
 
 
-%{
+
 %Code for generating true sinogram
 load('../PWLS-ULTRA-for-Low-Dose-3D-CT-Image-Reconstruction/data/3Dxcat/phantom_crop154.mat');  % unit of the loaded phantom: HU
 phantom = phantom(:,:,1:96); % extract 96 slices for testing
@@ -8,7 +8,7 @@ phantom = phantom(:,:,1:96); % extract 96 slices for testing
 fprintf('generating noiseless sino...\n');
 down = 1; % downsample rate
 ig_hi = image_geom('nx',840,'dx',500/1024,'nz',96,'dz',0.625,'down',down);
-down = 4; % downsample rate
+down = 8; % downsample rate
 cg = ct_geom('ge2', 'down', down);
 A_hi = Gcone(cg, ig_hi, 'type', 'sf2', 'nthread', jf('ncore')*2-1);  
 sino_true = A_hi * phantom;  clear A_hi;
@@ -24,7 +24,7 @@ down = 1;
 ig = image_geom('nx',420,'dx',500/512,'nz',96,'dz',0.625,'down',down);
 ig.mask = ig.circ > 0; % can be omitted
 
-down = 4; % downsample rate
+down = 8; % downsample rate
 cg = ct_geom('ge2', 'down', down);
 
 A = Gcone(cg, ig, 'type', 'sf2', 'nthread', jf('ncore')*2-1);
@@ -55,11 +55,18 @@ save('H_mask.mat', 'H')
 %}
 
 
-load('H_mask.mat', 'H')
-x_ncs = CT_3D(420, 96, 200, 1, 0.001, 0.1, 100, sino_true, A, ig.mask, H);
-%}
-%{
-x_pdhg = CT_3D(420, 96, 200, 1, 0.001, 0.1, 500, sino_true, A, ig.mask, false, []);
+%load('H_mask.mat', 'H')
+
+
+ek = zeros(420,420,96);
+ek(420/2,420/2,96/2) = 1;
+H = real(fftn(embed(A' * (A * ek(ig.mask(:))), ig.mask))./fftn(ek));
+
+
+x_ncs = CT_3D(420, 96, 200, 1, 0.001, 0.1, 0.01, sino_true, A, ig.mask, H);
+
+
+x_pdhg = CT_3D(420, 96, 200, 1, 0.001, 0.1, 1, sino_true, A, ig.mask, false);
 
 
 
